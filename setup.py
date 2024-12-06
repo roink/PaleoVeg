6906bcd0181d41262ca86f440223939d41fbfac6
Philipp Schlueter <p.schlueter@uni-koeln.de>
2024-12-06 12:49:02 +0100

    Add MANIFEST.in and some setup settings for publishing on pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup, find_packages
 
+# Read the contents of README.md
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="PaleoVeg",
-    version="0.1.0",
+    version="0.1.1",
     description="Predict paleovegetation from bioclimatic variables.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # Specify Markdown format
     author="Philipp Schlüter",
     author_email="p.schlueter@uni-koeln.de",
     url="https://github.com/roink/PaleoVeg",
@@ -21,6 +27,11 @@ setup(
     package_data={
         "paleoveg": ["model.pkl.bz2"],  # Include the model file
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     python_requires=">=3.7",
 )
 
